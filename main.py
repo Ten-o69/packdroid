bec1c34398fa6bbef39103e8c57359e5c5931f56
Ten-o69 <03822007sasha@gmail.com>
2025-09-24 18:15:41 +0300

    Now the check for installation of 'raccoon.jar' has been moved to the program entry point (main.py)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from common.helpers import init_check_paths
+from raccoon.install import check_raccoon_bin_install
 from install_apps import run_install
 
 if __name__ == "__main__":
     init_check_paths()
+    check_raccoon_bin_install()
+
     run_install()
--- raccoon/__init__.py
+++ raccoon/__init__.py
@@ -1,5 +1,1 @@
-from .install import check_raccoon_bin_install
-
-# Ensure that raccoon.jar binary is installed/available on import.
-# This check runs automatically when the raccoon package is imported.
-check_raccoon_bin_install()
+from .command import Raccoon
