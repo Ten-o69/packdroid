941f69cd7ab1443cb01dc99dd939ab05790e80f7
Ten-o69 <03822007sasha@gmail.com>
2025-09-24 15:03:21 +0300

    Added validation for the 'java_bin' configuration parameter. This parameter is used to specify the Java binary.

--- validation/config.py
+++ validation/config.py
@@ -1,13 +1,18 @@
+from typing import Union
+
 from pydantic import (
     BaseModel,
     HttpUrl,
     Field,
+    FilePath,
 )
 
 from common.constants import (
     WEB_LINK_DEFAULT_DOWNLOAD_BIN_RACCOON,
+    FILENAME_RACCOON_BIN,
 )
 
 
 class Config(BaseModel):
     raccoon_bin_link: HttpUrl = Field(default=WEB_LINK_DEFAULT_DOWNLOAD_BIN_RACCOON)
+    java_bin: Union[str, FilePath] = Field(default=FILENAME_RACCOON_BIN)
