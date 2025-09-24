f83cca13c8c9596ce5d9b91d848e529ceafc33ff
Ten-o69 <03822007sasha@gmail.com>
2025-09-24 14:19:01 +0300

    In the installation check function of the 'raccoon' utility, the installation algorithm has been replaced with the 'download_file' function from the common.helpers module.

--- raccoon/install.py
+++ raccoon/install.py
@@ -1,13 +1,12 @@
-import requests
-
 from common.constants import (
     DIR_BIN_RACCOON,
 )
+from common.helpers import download_file
 from config import cfg_obj
 
 def check_raccoon_bin_install():
     if not list(DIR_BIN_RACCOON.glob("raccoon.jar")):
-        file = requests.get(cfg_obj.raccoon_bin_link)
-
-        with open(DIR_BIN_RACCOON / "raccoon.jar", "wb") as raccoon_jar:
-            raccoon_jar.write(file.content)
+        download_file(
+            url=cfg_obj.raccoon_bin_link,
+            path=DIR_BIN_RACCOON / "raccoon.jar",
+        )
