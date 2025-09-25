edb722a4be398abe0db11156a0b0535aa4d4a3df
Ten-o69 <03822007sasha@gmail.com>
2025-09-25 10:29:39 +0300

    Added new variables with references for 'adb' binaries

--- common/constants.py
+++ common/constants.py
@@ -7,6 +7,7 @@ from pydantic import HttpUrl
 BASE_DIR = Path(__file__).parent.parent
 DIR_BIN = BASE_DIR / "bin"
 DIR_BIN_RACCOON = DIR_BIN / "raccoon"
+DIR_BIN_ADB = DIR_BIN / "adb"
 DIR_APKS = BASE_DIR / "apks"
 
 # path
@@ -40,5 +41,14 @@ FILENAME_RACCOON_BIN = "raccoon.jar"
 FILENAME_JAVA_BIN = "java"
 
 # web link
-WEB_LINK_DEFAULT_DOWNLOAD_BIN_RACCOON = HttpUrl("https://www.dropbox.com/scl/fi/8np6usic1qu2xisgtbpsh/"
-                                                "raccoon-4.24.0.jar?rlkey=jqzyn25ptmearic4gddd2762y&st=yt4vzyzr&dl=1")
+WEB_LINK_DEFAULT_DOWNLOAD_BIN_RACCOON: HttpUrl = HttpUrl("https://www.dropbox.com/scl/fi/8np6usic1qu2xisgtbpsh/"
+                                                         "raccoon-4.24.0.jar?rlkey=jqzyn25ptmearic4gddd2762y&st=yt4vzyzr&dl=1")
+WEB_LINK_DEFAULT_DOWNLOAD_BIN_ADB_FOR_WINDOWS: HttpUrl = HttpUrl("https://www.dropbox.com/scl/fi/48ra9r4rgliw1zt5pwe7z/"
+                                                                 "platform-tools-latest-windows.zip?rlkey="
+                                                                 "ayg3177h9fd9dqkd2nse6kwqa&st=24n561c3&dl=1")
+WEB_LINK_DEFAULT_DOWNLOAD_BIN_ADB_FOR_DARWIN: HttpUrl = HttpUrl("https://www.dropbox.com/scl/fi/0m421bmeahkwcky6z6udi/"
+                                                                "platform-tools-latest-darwin.zip?rlkey="
+                                                                "uh7pko39a7nehe6qxcotdo8h7&st=bi3epald&dl=1")
+WEB_LINK_DEFAULT_DOWNLOAD_BIN_ADB_FOR_LINUX: HttpUrl = HttpUrl("https://www.dropbox.com/scl/fi/nu3phl7bn8l2gn1syeuqk/"
+                                                               "platform-tools-latest-linux.zip?rlkey="
+                                                               "5sz131offsnuwci4x894529du&st=i0dgp5l7&dl=1")
